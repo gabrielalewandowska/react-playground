59f8b739d31e6e8bd2d88c0649d98e3190bb1fdb
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-11-26 19:59:23 +0000

    Rewrite conditional logic for toggling names

--- src/App.js
+++ src/App.js
@@ -41,25 +41,30 @@ class App extends Component {
       cursor: "pointer"
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, "Gaby")}>My Hobbies: Reading</Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            changed={this.nameChangedHandler} />
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle Names</button>
-       { 
-         this.state.showPersons ? 
-          <div>
-              <Person 
-                name={this.state.persons[0].name}   
-                age={this.state.persons[0].age}
-                click={this.switchNameHandler.bind(this, "Gaby")}>My Hobbies: Reading</Person>
-              <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              changed={this.nameChangedHandler} />
-            </div> : null
-        }
+        {persons}
       </div>
     );
   }
